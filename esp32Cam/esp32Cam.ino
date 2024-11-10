e20edfcb69291e2eeb32a1d50da284b0269e3e11
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-11-10 10:11:48 +0700

    updata hardware

--- esp32Cam/esp32Cam.ino
+++ esp32Cam/esp32Cam.ino
@@ -7,8 +7,8 @@
 // const char *password = "0902042212";
 const char *ssid = "zzz";
 const char *password = "123456789";
-const char* serverName = "http://192.168.22.22:8000/";
-const char* esp8266_url = "http://192.168.22.120/capture-complete";
+const char* serverNameHardwareApi = "http://192.168.120.9:8000/test/";
+const char* esp8266_url = "http://192.168.120.120/capture-complete";
 
 WebServer server(80);
 
@@ -33,10 +33,9 @@ WebServer server(80);
 #define HREF_GPIO_NUM  23
 #define PCLK_GPIO_NUM  22
 
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  // Serial.begin(9600);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -89,78 +88,51 @@ void setup() {
   }
 
   // Bắt đầu server
-  server.on("/capture", HTTP_GET, capture);  // Khi ESP8266 gửi lệnh chụp ảnh
+  server.on("/capture", HTTP_GET, capture);
   server.begin();
   Serial.println("HTTP server started");
 }
 
 void loop() {
-  
-  // // thay cai nay bang cam bien sieu am coi thu co nguoi hay khong
-  // if (Serial.available() > 0) {
-  //   String input = Serial.readStringUntil('\n'); // Đọc dòng nhập từ serial
-  //   input.trim();
-  //   if (input == "up") {
-  //     capture();
-  //     HTTPClient http;
-  //     String urlGet = String(serverName) + "result";
-  //     http.begin(urlGet); // URL API cho việc lấy kết quả
-  //     int httpResponseCode = http.GET();  // Gửi yêu cầu GET để lấy kết quả
-  //     if (httpResponseCode > 0) {
-  //     String result = http.getString();
-  //     Serial.println("Result: " + result);  // Hiển thị kết quả nhận diện
-
-
-  //   } else {
-  //     Serial.printf("Error on sending GET: %d\n", httpResponseCode);
-  //   }
-  //   http.end();
-  //   }else {
-  //     Serial.println("khong co nguoi dung gan thung rac");
-  //   }
-
-    
-  // }
   server.handleClient();
 }
 
 void capture() {
-  // Chụp ảnh và gửi lên server
+
   if (WiFi.status() == WL_CONNECTED) {
-    for (int i = 0; i < 3; i++) {  // Gửi 3 frame  
-          camera_fb_t * fb = esp_camera_fb_get();
-          if (!fb) {
-            Serial.println("Camera capture failed");
-            return;
-          }
+    for (int i = 0; i < 3; i++) {  // Gửi 3 frame
+      camera_fb_t * fb = esp_camera_fb_get();
+      if (!fb) {
+        Serial.println("Camera capture failed");
+        return;
+      }
 
-          HTTPClient http;
-          String urlPost = String(serverName) + "upload";
-          http.begin(urlPost);
-          http.addHeader("Content-Type", "image/jpeg");
-          http.addHeader("Content-Disposition", "attachment; filename=image.jpg");
-          http.addHeader("Content-Length", String(fb->len));
+      HTTPClient http;
+      // String urlPost = String(serverName) + "upload";
+      String urlPost = String(serverNameHardwareApi) + "upload";
+      http.begin(urlPost);
+      http.addHeader("Content-Type", "image/jpeg");
+      http.addHeader("Content-Disposition", "attachment; filename=image.jpg");
+      http.addHeader("Content-Length", String(fb->len));
 
-          int httpResponseCode = http.POST(fb->buf, fb->len);
-          if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println(httpResponseCode);
-            Serial.println(response);
-          } else {
-            Serial.printf("Error on sending POST: %d\n", httpResponseCode);
-          }
-
-          esp_camera_fb_return(fb);
-          http.end();
-          delay(100);  // Thêm delay giữa các lần gửi để tránh quá tải server
+      int httpResponseCode = http.POST(fb->buf, fb->len);
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+      } else {
+        Serial.printf("Error on sending POST: %d\n", httpResponseCode);
+      }
+      http.end();
+      esp_camera_fb_return(fb);
+      delay(100);
     }
     
-    // Gửi tín hiệu để ESP8266 nhận dữ liệu
     server.send(200, "text/plain", "Capture complete");
     notifyESP8266();
   }
-  
 }
+
 
 
 void notifyESP8266(){
@@ -175,3 +147,6 @@ void notifyESP8266(){
   }
   http.end();
 }
+
+
+
